347d0316bad724680e4b679c9d139bc3f915d13b
Bram Van der Sype <bram.vandersype@gmail.com>
2022-06-28 10:58:22 +0200

    Introduce PHP-DI for autowiring down the line

--- public/index.php
+++ public/index.php
@@ -2,18 +2,21 @@
 
 declare(strict_types=1);
 
-use Brammm\TestingWorkshop\Container;
 use Brammm\TestingWorkshop\Http\GetCustomers;
 use Brammm\TestingWorkshop\Http\GetOrders;
 use Brammm\TestingWorkshop\Http\GetProducts;
+use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
 use Slim\Psr7\Factory\ResponseFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$builder = new ContainerBuilder();
+$builder->addDefinitions(__DIR__ . '/../src/container.php');
+
 $app = AppFactory::create(
     new ResponseFactory(),
-    new Container(),
+    $builder->build()
 );
 
 $app->get('/customers', GetCustomers::class);
---
+++ src/container.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
+use Money\Currencies\ISOCurrencies;
+use Money\Formatter\IntlMoneyFormatter;
+use Money\MoneyFormatter;
+
+return [
+    Connection::class => fn () => DriverManager::getConnection([
+        'dbname' => 'testing-workshop',
+        'user' => 'root',
+        'password' => 'root',
+        'host' => 'php-testing-workshop-mysql',
+        'driver' => 'pdo_mysql',
+    ]),
+
+    MoneyFormatter::class => fn () => new IntlMoneyFormatter(
+        new NumberFormatter('nl_BE', NumberFormatter::CURRENCY),
+        new ISOCurrencies()
+    ),
+];
