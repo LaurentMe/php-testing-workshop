c253e78a721b209bb25b81a19f03f5a3932135a4
Bram Van der Sype <bram.vandersype@gmail.com>
2022-06-28 13:13:52 +0200

    Installed phpunit

---
+++ tests/MockTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brammm\TestingWorkshop;
+
+use PHPUnit\Framework\TestCase;
+
+final class MockTest extends TestCase
+{
+    public function testItRuns(): void
+    {
+        self::assertTrue(true);
+    }
+}
