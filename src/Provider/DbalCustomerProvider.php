85794ce6e87e026c50415e8e5321000c9e21b786
Bram Van der Sype <bram.vandersype@gmail.com>
2022-06-28 19:51:03 +0200

    Implement interfaces for providers

---
+++ src/Provider/DbalCustomerProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brammm\TestingWorkshop\Provider;
+
+use Brammm\TestingWorkshop\Model\Customer;
+use DateTimeImmutable;
+use Doctrine\DBAL\Connection;
+use Exception;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
+final class DbalCustomerProvider implements CustomerProvider
+{
+    public function __construct(
+        private readonly Connection $connection
+    ) {
+    }
+
+    public function findById(UuidInterface $uuid): Customer
+    {
+        $data = $this->connection->fetchAssociative('SELECT * FROM customer WHERE id = ?', [$uuid->toString()]);
+
+        if (! $data) {
+            throw new Exception('Not found');
+        }
+
+        return $this->hydrate($data);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function findAll(): array
+    {
+        return array_map(
+            [$this, 'hydrate'],
+            $this->connection->fetchAllAssociative('SELECT * FROM customer ORDER BY name')
+        );
+    }
+
+    /**
+     * @param array{id: string, name: string, customer_since: string} $row
+     */
+    private function hydrate(array $row): Customer
+    {
+        return new Customer(
+            Uuid::fromString($row['id']),
+            $row['name'],
+            new DateTimeImmutable($row['customer_since'])
+        );
+    }
+}
