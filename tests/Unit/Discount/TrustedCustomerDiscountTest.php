6ea096257ca6dbd718bce24fa8f9a4879d95a683
Bram Van der Sype <bram.vandersype@gmail.com>
2022-06-28 16:50:33 +0200

    Use description here as well

---
+++ tests/Unit/Discount/TrustedCustomerDiscountTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brammm\TestingWorkshop\Unit\Discount;
+
+use Brammm\TestingWorkshop\Discount\TrustedCustomerDiscount;
+use Brammm\TestingWorkshop\Model\Customer;
+use Brammm\TestingWorkshop\Model\Order;
+use Brammm\TestingWorkshop\Model\OrderLine;
+use Brammm\TestingWorkshop\Provider\CustomerProvider;
+use Brammm\TestingWorkshop\TestClock;
+use DateTimeImmutable;
+use Money\Money;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+final class TrustedCustomerDiscountTest extends TestCase
+{
+    private CustomerProvider|MockObject $customerProvider;
+
+    private TestClock $clock;
+
+    protected function setUp(): void
+    {
+        $this->customerProvider = $this->createMock(CustomerProvider::class);
+        $this->clock            = new TestClock();
+    }
+
+    public function testItReturnsZeroOnNewCustomers(): void
+    {
+        $discount = new TrustedCustomerDiscount(
+            $this->customerProvider,
+            $this->clock,
+        );
+
+        $customerId = Uuid::uuid4();
+
+        $this->customerProvider->expects($this->once())
+            ->method('findById')
+            ->willReturn(new Customer(
+                $customerId,
+                'John',
+                new DateTimeImmutable('2022-06-28')
+            ));
+        $this->clock->setNow(new DateTimeImmutable('2022-06-29'));
+
+        $discount = $discount->calculate(new Order(
+            Uuid::uuid4(),
+            $customerId,
+            [
+                new OrderLine('Some product', 1, Money::EUR(15000)),
+            ]
+        ));
+
+        self::assertEquals(Money::EUR(0), $discount);
+    }
+
+    public function testItReturnsDiscountForTrustedCustomers(): void
+    {
+        $discount = new TrustedCustomerDiscount(
+            $this->customerProvider,
+            $this->clock,
+        );
+
+        $customerId = Uuid::uuid4();
+
+        $this->customerProvider->expects($this->once())
+            ->method('findById')
+            ->willReturn(new Customer(
+                $customerId,
+                'John',
+                new DateTimeImmutable('2020-06-28')
+            ));
+        $this->clock->setNow(new DateTimeImmutable('2022-06-29'));
+
+        $discount = $discount->calculate(new Order(
+            Uuid::uuid4(),
+            $customerId,
+            [
+                new OrderLine('Some product', 1, Money::EUR(15000)),
+            ]
+        ));
+
+        self::assertEquals(Money::EUR(3000), $discount);
+    }
+}
