6e35d77bbdd269c38bc3f251bf17a1b46f85a5a7
LaurentMe <laurent.meersman@gmail.com>
2022-06-29 13:16:31 +0200

    Install phpunit + setup first test

---
+++ tests/Unit/MockTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brammm\TestingWorkshop\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class MockTest extends TestCase
+{
+    public function testPHPUnitWorks(): void
+    {
+        self::assertTrue(true);
+    }
+}
